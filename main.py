ac47681694cfc60583542bcb6372f72d1bb1fed8
Beau Rogers <brogers1812@gmail.com>
2021-05-17 18:37:44 -0700

    Included male crunches and score

--- main.py
+++ main.py
@@ -60,7 +60,26 @@ else:
 pullup=str(pullup)
 
 #read and setup male pullup CSV files
-m_pullup_df=pd.read_csv("m_pull.csv",index_col=0)
+m_pullup_df=pd.read_csv("lookup_records\csv\m_pull.csv",index_col=0)
 m_pull_pts = m_pullup_df.loc[[pullup],[years]].values[0]
 
 print(int(m_pull_pts))
+
+#Recieve total crunches from user
+crunch = int(input("How many crunches?:  "))
+if type(crunch) == int:
+    print("You performed {} crunches".format(crunch))
+else:
+    print("That's not a valid response.")
+
+crunch=str(crunch)
+
+#read and setup male crunch CSV files
+m_crunch_df=pd.read_csv("lookup_records\csv\m_crunch.csv",index_col=0)
+m_crunch_pts = m_crunch_df.loc[[crunch],[years]].values[0]
+
+print(int(m_crunch_pts))
+
+total = int(m_crunch_pts) ++ int(m_crunch_pts)
+
+print("Your total score is {} out of 300.".format(total))
--- test.py
+++ test.py
@@ -1,12 +1,27 @@
 import os
 import pandas as pd
 
-reps = str(17)
-years = str(35)
+reps = str(95)
+years = str(17)
 
 #read and setup male pullup CSV files
-m_pullup_df=pd.read_csv("m_pull.csv",index_col=0)
+m_pullup_df=pd.read_csv("m_crunch.csv",index_col=0)
 m_pull_pts = m_pullup_df.loc[[reps],[years]].values[0]
 
 
 print(int(m_pull_pts))
+
+#Recieve total crunches from user
+crunch = int(input("How many crunches?:  "))
+if type(crunch) == int:
+    print("You performed {} crunches".format(crunch))
+else:
+    print("That's not a valid response.")
+
+crunch=str(crunch)
+
+#read and setup male crunch CSV files
+m_crunch_df=pd.read_csv("m_crunch.csv",index_col=0)
+m_crunch_pts = m_crunch_df.loc[[crunch],[years]].values[0]
+
+print(int(m_crunch_pts))
